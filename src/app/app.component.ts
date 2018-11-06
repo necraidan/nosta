6e3b34ae5a74b548c3354597093b05f5a5c03183
BuildTools <benjamin.auzanneau.tm@gmail.com>
2018-11-07 00:56:09 +0100

    Alpha

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatSort, MatTableDataSource } from '@angular/material';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'nosta';
+export class AppComponent implements OnInit {
+    displayedColumns: string[] = ['nom', 'date', 'ged'];
+    dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+    searchForm = new FormGroup({
+        query: new FormControl('')
+    });
+
+    @ViewChild(MatSort)
+    sort: MatSort;
+
+    constructor(private _http: HttpClient) {}
+
+    ngOnInit(): void {
+        this.dataSource.sort = this.sort;
+        this.searchForm.valueChanges.subscribe(values => {
+            this.dataSource.filter = values.query;
+        });
+    }
 }
+
+export interface PeriodicElement {
+    nom: string;
+    date: string;
+    ged?: boolean;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+    // tslint:disable-next-line:quotemark
+    { nom: "MacDonald's", date: '02/01/2005', ged: true },
+    { nom: 'KFC', date: '10/10/2018', ged: false },
+    { nom: 'Subway', date: '01/07/1997' },
+    { nom: 'Burger King', date: '22/12/2019' }
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import {
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatSortModule,
+    MatTableModule,
+    MatToolbarModule
+} from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        ReactiveFormsModule,
+        BrowserModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        MatCheckboxModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatTableModule,
+        MatSortModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
