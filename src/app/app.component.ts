c693bbc788ee267de85c16bfbfacb32e0141880a
BuildTools <benjamin.auzanneau.tm@gmail.com>
2021-02-22 22:09:54 +0100

    maj angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,45 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { MatSort, MatTableDataSource } from '@angular/material';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-    displayedColumns: string[] = ['nom', 'date', 'ged'];
-    dataSource = new MatTableDataSource(ELEMENT_DATA);
+  displayedColumns: string[] = ['nom', 'date', 'ged'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
-    searchForm = new FormGroup({
-        query: new FormControl('')
+  searchForm = new FormGroup({
+    query: new FormControl(''),
+  });
+
+  @ViewChild(MatSort)
+  sort!: MatSort;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.dataSource.sort = this.sort;
+    this.searchForm.valueChanges.subscribe((values) => {
+      this.dataSource.filter = values.query;
     });
-
-    @ViewChild(MatSort)
-    sort: MatSort;
-
-    constructor(private http: HttpClient) {}
-
-    ngOnInit(): void {
-        this.dataSource.sort = this.sort;
-        this.searchForm.valueChanges.subscribe(values => {
-            this.dataSource.filter = values.query;
-        });
-    }
+  }
 }
 
 export interface CompanyElement {
-    nom: string;
-    date: string;
-    ged?: boolean;
+  nom: string;
+  date: string;
+  ged?: boolean;
 }
 
 const ELEMENT_DATA: CompanyElement[] = [
-    // tslint:disable-next-line:quotemark
-    { nom: "MacDonald's", date: '02/01/2005', ged: true },
-    { nom: 'KFC', date: '10/10/2018', ged: false },
-    { nom: 'Subway', date: '01/07/1997' },
-    { nom: 'Burger King', date: '22/12/2019' }
+  // tslint:disable-next-line:quotemark
+  { nom: "MacDonald's", date: '02/01/2005', ged: true },
+  { nom: 'KFC', date: '10/10/2018', ged: false },
+  { nom: 'Subway', date: '01/07/1997' },
+  { nom: 'Burger King', date: '22/12/2019' },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,35 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import {
-    MatButtonModule,
-    MatCheckboxModule,
-    MatFormFieldModule,
-    MatIconModule,
-    MatInputModule,
-    MatSortModule,
-    MatTableModule,
-    MatToolbarModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [
-        ReactiveFormsModule,
-        BrowserModule,
-        HttpClientModule,
-        BrowserAnimationsModule,
-        MatCheckboxModule,
-        MatToolbarModule,
-        MatButtonModule,
-        MatFormFieldModule,
-        MatIconModule,
-        MatInputModule,
-        MatTableModule,
-        MatSortModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [
+    ReactiveFormsModule,
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatTableModule,
+    MatSortModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
